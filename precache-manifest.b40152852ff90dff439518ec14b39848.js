578af7cf4f2007310061a5c098459265d5197c61
Juan Cerrutti <jicerrutti@gmail.com>
2019-12-19 09:50:41 -0300

    Updates

---
+++ precache-manifest.b40152852ff90dff439518ec14b39848.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e938f90e4c6523853a3d6b365d089265",
+    "url": "/shapefity/index.html"
+  },
+  {
+    "revision": "0a64f3696fc54537a2f3",
+    "url": "/shapefity/static/css/main.edda920f.chunk.css"
+  },
+  {
+    "revision": "693647d526ee3ef19509",
+    "url": "/shapefity/static/js/2.320b7db8.chunk.js"
+  },
+  {
+    "revision": "29c8d19450d117745c250b1131e2dada",
+    "url": "/shapefity/static/js/2.320b7db8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a64f3696fc54537a2f3",
+    "url": "/shapefity/static/js/main.df5d4821.chunk.js"
+  },
+  {
+    "revision": "aff62251b210f474168f",
+    "url": "/shapefity/static/js/runtime-main.22a0d86e.js"
+  }
+]);
